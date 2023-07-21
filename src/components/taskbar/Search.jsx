b8f9144a4287b9477b073a5f3cf312cc082761f6
cabraja <cabrajamihajlo@gmail.com>
2023-07-21 22:46:55 +0200

    complete search

--- src/components/taskbar/Search.jsx
+++ src/components/taskbar/Search.jsx
@@ -2,10 +2,13 @@ import {LiaSearchSolid} from 'react-icons/lia'
 import { useState } from 'react'
 import {RiCloseFill} from 'react-icons/ri'
 import SearchIcon from './SearchIcon'
+// ICON IMAGES
 import resume from '../../assets/images/resume-small.jpg'
 import info from '../../assets/images/info.png'
 import skills from '../../assets/images/skills.png'
 import folder from '../../assets/images/folder.png'
+import github from '../../assets/images/github-mark-white.png'
+import linkedin from '../../assets/images/linkedin-logo.png'
 // HOOKS
 import useResumeWindow from "../../hooks/useResumeWindow"
 import useAboutWindow from "../../hooks/useAboutWindow"
@@ -20,6 +23,44 @@ function Search() {
   const projectsWindow = useProjectsWindow()
 
   const [isSearchOpen, setIsSearchOpen] = useState(false)
+  const [keyword, setKeyword] = useState('')
+
+  let icons = [
+    {
+      title: 'Github',
+      img: github,
+      onClick: null,
+      url: 'https://github.com/cabraja'
+    },
+    {
+      title: 'LinkedIn',
+      img: linkedin,
+      onClick: null,
+      url: 'https://www.linkedin.com/in/mcabraja/'
+    },
+    {
+      title: 'My Resume',
+      img: resume,
+      onClick: resumeWindow.onOpen,
+    },
+    {
+      title: 'About Me',
+      img: info,
+      onClick: aboutWindow.onOpen,
+    },
+    {
+      title: 'Skills',
+      img: skills,
+      onClick: skillsWindow.onOpen,
+    },
+    {
+      title: 'My Projects',
+      img: folder,
+      onClick: projectsWindow.onOpen,
+    },
+  ]
+
+  icons = icons.filter(item => item.title.toLowerCase().includes(keyword.toLowerCase()));
 
   return (
     <div className='
@@ -45,10 +86,16 @@ function Search() {
         onClick={() => setIsSearchOpen(true)}>
           <LiaSearchSolid size={20}/>
 
-          <input type='text' placeholder='Type here to search' className='
-          ps-3
-          outline-none
-          '/>
+          <input 
+            type='text'
+            placeholder='Type here to search'
+            className='
+            ps-3
+            outline-none
+          '
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          />
         </div>
 
         <div className={`
@@ -62,6 +109,7 @@ function Search() {
         transition
         duration-300
         backdrop-blur
+        overflow-y-scroll
         ${isSearchOpen ? "translate-y-0" : "translate-y-[200%]"}
         ${isSearchOpen ? "opacity-100" : "opacity-0"}
         `}>
@@ -79,6 +127,10 @@ function Search() {
             items-center
             justify-end
             p-1
+            sticky
+            top-0
+            bg-neutral-800
+            z-50
             '
             onClick={() => setIsSearchOpen(false)}>
               <RiCloseFill className='transition hover:text-neutral-400' size={26}/>
@@ -91,10 +143,30 @@ function Search() {
             px-2
             text-white
             '>
-              <SearchIcon title={'My Resume'} image={resume} onClick={resumeWindow.onOpen}/>
-              <SearchIcon title={'About Me'} image={info} onClick={aboutWindow.onOpen}/>
-              <SearchIcon title={'Skills'} image={skills} onClick={skillsWindow.onOpen}/>
-              <SearchIcon title={'My Projects'} image={folder} onClick={projectsWindow.onOpen}/>
+              {
+                icons.length > 0
+                ?
+                icons.map((item,index) => {
+                  if(!item.url) return <SearchIcon title={item.title} image={item.img} key={index} onClick={item.onClick}/>
+
+                  return (
+                    <a href={item.url} key={index} target='_blank'>
+                      <SearchIcon title={item.title} image={item.img} onClick={item.onClick}/>
+                    </a>
+                  )
+                })
+                :
+                <div className='
+                h-[28vh]
+                w-full
+                flex
+                flex-col
+                items-center
+                justify-center
+                '>
+                  <p className='text-neutral-500 mt-5 text-center'>Nothing found...</p>
+                </div>
+              }
             </div>
 
           </div>
